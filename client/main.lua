33a996de8576e4766e7b015c2617c50ced4f47a7
Outsider <87246847+outsider31000@users.noreply.github.com>
2024-11-06 19:43:54 +0000

    fix(client): inputs

--- client/main.lua
+++ client/main.lua
@@ -1,9 +1,27 @@
-local MenuData = exports.vorp_menu:GetMenuData()
-local Core = exports.vorp_core:GetCore()
+local MenuData <const> = exports.vorp_menu:GetMenuData()
+local Core <const> = exports.vorp_core:GetCore()
 
 RegisterNetEvent("vorp_billing:client:openMenu", function()
     OpenBillingMenu()
 end)
+
+local function myInput(header, placeholder, type, pattern, title)
+    local input <const> = {
+        type = "enableinput",
+        inputType = "input",
+        button = "Confirm",
+        placeholder = placeholder,
+        style = "block",
+        attributes = {
+            inputHeader = header,
+            type = type,
+            pattern = pattern,
+            title = title,
+            style = "border-radius: 10px; background-color: ; border:none;",
+        }
+    }
+    return input
+end
 
 function OpenBillingMenu()
     MenuData.CloseAll()
@@ -11,11 +29,11 @@ function OpenBillingMenu()
     local reason = ""
     local amount = 0
 
-    local elements = {
-        { label = "Player ID", value = "playerId", desc = "The ID of the player you want to bill" },
-        { label = "Reason",    value = "reason",   desc = "The reason for the bill" },
-        { label = "Amount",    value = "amount",   desc = "The amount of money to bill" },
-        { label = "Confirm",   value = "confirm",  desc = "Submit the bill" },
+    local elements <const> = {
+        { label = "Player ID",   value = "playerId", desc = "The ID of the player you want to bill" },
+        { label = "Bill Reason", value = "reason",   desc = "The reason for the bill" },
+        { label = "Bill Amount", value = "amount",   desc = "The amount of money to bill" },
+        { label = "Confirm",     value = "confirm",  desc = "Submit the bill" },
     }
 
     MenuData.Open("default", GetCurrentResourceName(), "OpenBillingMenu", {
@@ -26,43 +44,51 @@ function OpenBillingMenu()
         elements = elements
 
     }, function(data, menu)
-        local myinput = {
-            type = "input",
-            title = "Player ID",
-            description = "The ID of the player you want to bill",
-        }
-
-        local result = exports.vorp_input:GetInput(myinput)
-        if not result then return end
-
-        if data.current.value == "playerId" and result > 0 then
-            menu.setElement(1, "label", "Player ID<br> <b> Added ID:" .. result)
-            menu.setElement(1, "desc", "The ID of the player you want to bill")
-            playerId = result
-        end
-
-        if data.current.value == "reason" then
-            menu.setElement(2, "label", "Bill Reason<br> <b>Bill reason added")
-            menu.setElement(2, "desc", "Bill reason: " .. result)
-            reason = result
-        end
-
-        if data.current.value == "amount" then
-            menu.setElement(3, "label", "Amount added <br> <b>Amount:" .. result)
-            menu.setElement(3, "desc", "The amount of money to bill")
-            amount = result
-        end
-
         if data.current.value == "confirm" then
             if playerId <= 0 or reason == "" or amount <= 0 then
                 return Core.NotifyObjective("Please fill in all fields", 5000)
             end
+
+            menu.close()
+
             local info = {
                 playerId = playerId,
                 reason = reason,
                 amount = amount,
             }
-            TriggerServerEvent("vorp_billing:server:SendBill", info)
+            return TriggerServerEvent("vorp_billing:server:SendBill", info)
+        end
+
+        local type <const> = (data.current.value == "playerId" or data.current.value == "amount") and "number" or "text"
+        local pattern <const> = (data.current.value == "playerId" or data.current.value == "amount") and "[0-9]" or   "[a-zA-Z]"
+        local title <const> = (data.current.value == "playerId" or data.current.value == "amount") and "Only Numbers Are Allowed" or "Only Letters Are Allowed"
+        local input <const> = myInput("Billing Menu", "type here", type, pattern, title)
+
+        local result <const> = exports.vorp_inputs:advancedInput(input)
+        if not result then return end
+
+        if data.current.value == "playerId" and result > 0 then
+            menu.setElement(1, "label", "Player ID<br> <b> Added ID:" .. result)
+            menu.setElement(1, "desc", "The ID of the player you want to bill")
+            menu.refresh()
+            playerId = result
+        end
+
+        if data.current.value == "reason" then
+            menu.setElement(2, "label", "Bill Reason<br>bill reason added")
+            menu.setElement(2, "desc", "Bill reason: " .. result)
+            menu.refresh()
+            reason = result
+        end
+
+        if data.current.value == "amount" then
+            if result > Billing.MaxBillAmount then
+                return Core.NotifyObjective("You can not bill more than " .. Billing.MaxBillAmount, 5000)
+            end
+            menu.setElement(3, "label", "Bill Amount <br>Amount added: " .. result)
+            menu.setElement(3, "desc", "The amount of money to bill")
+            menu.refresh()
+            amount = result
         end
     end)
 end
