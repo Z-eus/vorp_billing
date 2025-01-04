86769f3e8d92ecea49e70d8eee93431b1add5953
Outsider <87246847+outsider31000@users.noreply.github.com>
2025-01-04 03:41:28 +0000

    chore(server): fix indentation

--- server/main.lua
+++ server/main.lua
@@ -1,7 +1,7 @@
 local Core <const> = exports.vorp_core:GetCore()
 local T <const> = Translation.Langs[Billing.Lang] -- Load the active language for the server
 
-RegisterCommand(Billing.Command, function(source, args, rawCommand)
+RegisterCommand(Billing.Command, function(source)
     local user <const> = Core.getUser(source)
     if not user then return end
 
@@ -68,15 +68,8 @@ RegisterNetEvent("vorp_billing:server:SendBill", function(data)
 
     if Billing.AllowBillingNegative then
         target.getUsedCharacter.addCurrency(0, data.amount)
-        Core.NotifyObjective(_source,
-            T.Notifications.bill_successful ..
-            " " ..
-            target.getUsedCharacter.firstname ..
-            " " .. target.getUsedCharacter.lastname .. " " .. T.Notifications.For .. " " .. data.amount, 5000)
-        Core.NotifyObjective(data.playerId,
-            T.Notifications.bill_received ..
-            data.amount ..
-            " " .. T.ReceiptInfo.billed_by .. " " .. charname .. " " .. T.Notifications.For .. " " .. data.reason, 5000)
+        Core.NotifyObjective(_source, T.Notifications.bill_successful .. " " .. target.getUsedCharacter.firstname .. " " .. target.getUsedCharacter.lastname .. " " .. T.Notifications.For .. " " .. data.amount, 5000)
+        Core.NotifyObjective(data.playerId, T.Notifications.bill_received .. data.amount .. " " .. T.ReceiptInfo.billed_by .. " " .. charname .. " " .. T.Notifications.For .. " " .. data.reason, 5000)
     else
         if sourceCharacter.money < data.amount then
             return Core.NotifyObjective(_source, T.Notifications.insufficient_funds, 5000)
@@ -118,6 +111,5 @@ RegisterNetEvent("vorp_billing:server:SendBill", function(data)
         "**" .. Logs.Lang.TargetSteam .. "** " .. targetSteamname .. "\n" ..
         "**" .. Logs.Lang.TargetID .. "** " .. targetIdentifier
 
-    Core.AddWebhook(Logs.Lang.BillSent, Logs.Webhook, description, Logs.color, Logs.Namelogs, Logs.logo, Logs.footerlogo,
-        Logs.avatar)
+    Core.AddWebhook(Logs.Lang.BillSent, Logs.Webhook, description, Logs.color, Logs.Namelogs, Logs.logo, Logs.footerlogo, Logs.avatar)
 end)
