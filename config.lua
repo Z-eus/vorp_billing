4337d631b5ea6b3bf40ffedab4f727e331556dc5
Zeus Script <51821404+Z-eus@users.noreply.github.com>
2025-01-18 19:27:36 +0300

    "type here" text moved to `translations.lua`

--- config.lua
+++ config.lua
@@ -1,15 +1,15 @@
 Billing = {}
-Billing.Lang = "English"                         -- language you want to use please make sure its in the translation.lua
+Billing.Lang = "English"            -- Language you want to use please make sure its in the translation.lua
 
-Billing.GiveMoneyToJob = true       -- if false the money wont be given to anyone if true the money will be given to the person who is billing
+Billing.GiveMoneyToJob = true       -- If false the money wont be given to anyone if true the money will be given to the person who is billing
 
-Billing.AllowBillingNegative = true -- if true it will alow to bill player to negative money, if true you cant bill players that dont have money
+Billing.AllowBillingNegative = true -- If true it will alow to bill player to negative money, if true you cant bill players that dont have money
 
-Billing.GiveReceipt = true          -- if true the player who got billed will receive a receipt as item
+Billing.GiveReceipt = true          -- If true the player who got billed will receive a receipt as item
 
-Billing.ReceiptItem = "receipt"     -- the item that will be given to the player who paid the bill, add this item to the database
+Billing.ReceiptItem = "receipt"     -- The item that will be given to the player who paid the bill, add this item to the database
 
-Billing.ServerYear = 1899           -- the year that will be used in the receipt description
+Billing.ServerYear = 1899           -- The year that will be used in the receipt description
 
 -- jobs allowed to use billing and ranks
 Billing.Jobs = {
@@ -22,12 +22,12 @@ Billing.Jobs = {
     Shaman = 0,
 }
 
--- the command that will be used to bill players
+-- The command that will be used to bill players
 Billing.Command = "bill"
 
-Billing.MaxBillAmount = 1000 -- players can not be billed more than this amount
+Billing.MaxBillAmount = 1000 -- Players can not be billed more than this amount
 
---server side function to check if player is onduty
+-- Server Side function to check if player is onduty
 local isServer = IsDuplicityVersion()
 if isServer then
     Billing.GetIsOnduty = function(source)
